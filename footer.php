f8e50cb73be9fe5d8b8f9169f5a52f506883c163
GelloCruz <71957801+GelloCruz@users.noreply.github.com>
2022-07-26 00:37:38 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,55 @@
+<!-- 
+  <div class="banner-card">
+    <img src="bg1.jpg">
+  </div> -->
+
+
+
+<footer>
+   <div class="content">
+     <div class="left box">
+       <div class="upper">
+         <div class="topic">Life in Christ</div>
+         <p>We are willing to help you for our purpose is to 
+            follow and obey God's will to serve people.
+         </p>
+       </div>
+       <div class="lower">
+         <div class="topic">Contact us</div>
+         <div class="phone">
+           <a href="#" style="text-decoration: none; color: rgb(138, 138, 138);"><i class="fas fa-phone-volume"></i>+63 906 241 2860</a>
+         </div>
+         <div class="email">
+           <a href="#" style="text-decoration: none; color: rgb(138, 138, 138);"><i class="fas fa-envelope"></i>lifeinchrist@gmail.com</a>
+         </div>
+       </div>
+     </div>
+     <div class="middle box">
+       <div class="topic">Useful Link</div>
+       <div><a href="#">About Us</a></div>
+       <div><a href="#">Events</a></div>
+       <div><a href="#">Preaching</a></div>
+       <div><a href="#">Ministries</a></div>
+       <div><a href="#">Connect us</a></div>
+     </div>
+     <div class="right box">
+       <div class="topic">Get in Touch</div>
+       <form action="#">
+         <input type="text" placeholder="Enter your Name">
+         <input type="text" placeholder="Enter your Message">
+         <input type="submit" name="submit" value="Send">
+         <div class="media-icons">
+           <a href="#"><i class="fab fa-facebook-f" style="margin-top:10px;"></i></a>
+           <a href="#"><i class="fab fa-instagram" style="margin-top:10px;"></i></a>
+           <a href="#"><i class="fab fa-twitter" style="margin-top:10px;"></i></a>
+           <a href="#"><i class="fab fa-youtube" style="margin-top:10px;"></i></a>
+         </div>
+       </form>
+     </div>
+   </div>
+   <div class="bottom">
+     <p>Copyright © 2022 <a href="about.php" style="text-decoration:none;">Life in Christ</a> All rights reserved</p>
+   </div>
+ </footer>
+
+  
---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Life in Christ </title>
+    <link rel = "icon" href = "images/bg2.jpg" type = "image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"> </script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"> </script>
+    <!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    
+    
+</head>
+<body>
+    <style>
+            /* Font */
+@import url('https://fonts.googleapis.com/css?family=Quicksand:400,700');
+body {
+  color: #272727;
+  font-family: 'Quicksand', serif;
+  font-style: normal;
+  font-weight: 400;
+
+
+}
+ nav {
+    overflow: hidden;
+    background-color: #272727;
+    position: fixed;
+    top: 0;
+    height: 80px;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+}
+nav ul li a {
+    color: orangered;
+    font-size: 17px;
+    padding: 7px 13px;
+    text-decoration: none;
+}
+a:hover {
+    color: orangered;
+}
+h1 {
+    font-size: 24px;
+    font-weight: 400;
+    text-align: center;
+}
+    </style>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"> </i>
+        </label>
+
+        <label class="logo"><img src="images/bg.jpg" style="width:50px; height: 50px; border-radius: 50%;"> Life in Christ </label>
+        
+    <ul>
+        <li> <a class="active" href="about.php">About Us </a></li>
+        <li> <a href="event.php"> Events </a></li>
+        <li> <a href="preaching.php"> Preaching</a></li>
+        <li> <a href="#eventoverall"> Ministries </a></li>
+        <li> <a href="#overallconnectus"> Connect Us </a></li>
+    </ul>
+    </nav>
+
+    
