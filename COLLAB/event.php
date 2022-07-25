1277e861dac5597a1a419561eed0b20349d94199
GelloCruz <71957801+GelloCruz@users.noreply.github.com>
2022-07-26 00:35:10 +0800

    Add files via upload

---
+++ COLLAB/event.php
@@ -0,0 +1,80 @@
+<title>Life in Christ | Event</title>
+<?php
+include 'header.php';
+
+
+?>
+<title>Life in Christ | Preaching</title>
+    <div class="event">
+        <div class="eventoverall">
+            <div class="eventtitle">
+                <h4> Church Event </h4>
+            </div>
+          
+        </div>
+    </div>
+
+    
+        <div class="container">
+        <div class="index-post">
+        <h1>Upcoming Events</h1>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg3.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Encounter</h2>
+                <span class="desc">November 2, 2022</span>
+            </div>
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg1.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Youth Camp</h2>
+                <span class="desc">January 23, 2023</span>
+            </div>
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg2.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Retreat</h2>
+                <span class="desc">February 17, 2023</span>
+            </div>
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg2.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Ablaze</h2>
+                <span class="desc">March 3, 2023</span>
+            </div>
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        </div>
+  
+        </div>
+      
+<!-- FOOTER -->
+            <?php
+            include 'footer.php';
+
+            ?>
+       
+</body>
+</html>
