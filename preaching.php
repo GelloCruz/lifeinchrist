ab56439862a05a709072b55e8f57a60d9214a8e0
GelloCruz <gellocruz14@gmail.com>
2022-07-25 23:56:19 +0800

    LifeinChrist
    First Project

---
+++ preaching.php
@@ -0,0 +1,144 @@
+<title>Life in Christ | Preaching</title>
+<?php
+include 'header.php';
+
+
+?>
+
+    <div class="preaching">
+        <div class="preachingoverall">
+            <div class="preachingtitle">
+                <h4 style="text-align:left"> Watch Life in Christ Church
+                latest preaching. </h4><br>
+                <h5 style="text-align:left">Browse our archive and watch
+                    videos on any of your devices.</h5>
+            </div>
+          
+        </div>
+    </div>
+    
+        <div class="container">
+        <div class="index-post">
+        <h1>Sunday Services</h1>
+        <div class="button-watch">
+                <a href="#" class="button">Watch All Services</a>
+            </div>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg3.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Love of God</h2>
+                <span class="desc">August 22, 2022</span>
+            </div>
+            
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg1.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Turning Point</h2>
+                <span class="desc">August 28, 2022</span>
+            </div>
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg2.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Goodness of God</h2>
+                <span class="desc">September 8, 2022</span>
+            </div>
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg2.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Set us Free</h2>
+                <span class="desc">September 13, 2022</span>
+            </div>
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        
+        </div>
+
+
+        <!-- ABLAZE -->
+        <div class="index-post">
+        <h1>Ablaze</h1>
+        <div class="button-watch">
+                <a href="#" class="button">Watch All Ablaze</a>
+            </div>
+            <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg3.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Love of God</h2>
+                <span class="desc">August 22, 2022</span>
+            </div>
+            
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg1.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Turning Point</h2>
+                <span class="desc">August 28, 2022</span>
+            </div>
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg2.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Goodness of God</h2>
+                <span class="desc">September 8, 2022</span>
+            </div>
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        <div class="contener">
+            <div class="image-contener">
+                <img src="images/bg2.jpg" alt="">
+            </div>
+            <div class="meta-contener">
+                <h2 class="title">Set us Free</h2>
+                <span class="desc">September 13, 2022</span>
+            </div>
+            <div class="button-contener">
+                <a href="#" class="button">Read More →</a>
+            </div>
+        </div>
+        </div>
+        </div>
+      <br>
+<!-- FOOTER -->
+            <?php
+            include 'footer.php';
+
+            ?>
+       
+</body>
+</html>
